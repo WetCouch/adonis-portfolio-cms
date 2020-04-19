4dc7987da0edd946d19ad847b21b0a318b5d04ff
Wojciech Janoszek <wjanoszek@icloud.com>
2020-04-19 21:09:20 +0200

    Project card animation

--- resources/assets/scripts/projectInteraction.js
+++ resources/assets/scripts/projectInteraction.js
@@ -1,7 +1,17 @@
-const projects = document.getElementsByClassName('projects__card');
+const homepageProjects = document.getElementsByClassName('projects__card');
+const portfolioProjects = document.getElementsByClassName('project-card');
 
-Array.from(projects).forEach(project => {
+if (homepageProjects) Array.from(homepageProjects).forEach(project => {
   const style = 'projects__card--active';
+
+  project.addEventListener('click', () => {
+    if (project.classList.contains(style)) project.classList.remove(style);
+    else project.classList.add(style);
+  })
+});
+
+if (portfolioProjects) Array.from(portfolioProjects).forEach(project => {
+  const style = 'project-card--active';
 
   project.addEventListener('click', () => {
     if (project.classList.contains(style)) project.classList.remove(style);
